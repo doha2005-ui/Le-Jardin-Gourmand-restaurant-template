960c681e02559c0128c899d46afd68e48638ebb0
aya <ayaabk5002@gmail.com>
2026-02-21 20:33:31 +0000

    add Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,1 +1,30 @@
- 
+import React from 'react'
+import './style.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <ul className='icon'>
+            <li><ion-icon name="logo-facebook"></ion-icon></li>
+            <li><ion-icon name="logo-instagram"></ion-icon></li>
+            <li><ion-icon name="logo-twitter"></ion-icon></li>
+            <li><ion-icon name="logo-linkedin"></ion-icon></li>
+            <li><ion-icon name="logo-youtube"></ion-icon></li>
+        </ul>
+        <ul className='menu'>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/"> A propos </a></li>
+            <li><a href="/">  Blog </a></li>
+            <li><a href="/">Contact </a></li>
+            <li><a href="/"> Mentions légales </a></li>
+        
+        </ul>
+        <hr />
+        <p>&copy;{new Date().getFullYear()} Tous droits réservés.</p>
+    </footer>
+    )
+
+  
+}
+
+export default Footer
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,1 +1,39 @@
- 
+ import React, { useEffect, useState } from 'react'
+import img from "./5845342.png";
+import "./nav.css"
+
+const Header = () => {
+  const [scroll,setScroll]=useState(false)
+  useEffect(()=>{
+  function handleScroll(){
+    if (window.scrollY>100){
+      setScroll(true)
+    }
+    else{
+      setScroll(false)
+    }
+  }
+  document.addEventListener("scroll",handleScroll)
+  return()=>{
+    window.removeEventListener("scroll",handleScroll)
+  }},[])
+  return (
+    <header className={`header ${scroll? "try":""}`}>
+      <div className='logo-container'>
+        <img src={img} alt="logo" className='img' />
+        <a href="/" className='logo'>Le jardin gourmand</a>
+
+      </div>
+
+
+      <nav className='navbar'>
+        <a href="/">Accueil</a>
+        <a href="/">A propos</a>
+        <a href="/">Blog</a>
+        <a href="/">Contact</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
